f5494aa15d3ae4b998691af10aa83005b45e7128
Rifah Fatima <rifah22006@gmail.com>
2026-07-19 19:27:53 +0400

    preliminary dataset

--- docs.py
+++ docs.py
@@ -1,12 +1,13 @@
 import arxiv 
 from urllib.request import urlretrieve
 import os
-import requests
+
+
 client_search = arxiv.Client()
 
 search = arxiv.Search(
-    query="Endangered animal species", 
-    max_results=100,
+    query="Endangered animals OR species", 
+    max_results=200,
     sort_by= arxiv.SortCriterion.Relevance
 )
 
@@ -17,14 +18,3 @@ for result in results:
     print("result ", result.title)
     urlretrieve(result.pdf_url, f"./docs/{result.title}.pdf")
 
-
-print("now openalex\n")
-oa_api = os.getenv("OA_API_KEY")
-
-url = f"https://api.openalex.org/works?api_key={oa_api}"
-
-payload = {"search": "endangered species research", "filter": "is_oa:true"}
-
-r = requests.get(url, params=payload)
-
-print(r.json())
