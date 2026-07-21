863582db52a85a618b22fb220c954edb8d0936ad
Rifah Fatima <rifah22006@gmail.com>
2026-07-21 16:02:22 +0400

    ingestion

---
+++ rag.py
@@ -0,0 +1,21 @@
+from llama_index.core import SimpleDirectoryReader
+from llama_parse import LlamaParse
+import nest_asyncio
+import os
+nest_asyncio.apply()
+from dotenv import load_dotenv
+
+load_dotenv()
+first_api = os.getenv("LLAMA_CLOUD_API_KEY")
+
+parser = LlamaParse(api_key = first_api
+                    ,result_type="markdown", user_prompt="Make sure the structure of all info is preserved",
+                    extract_charts=True,auto_mode_trigger_on_table_in_page=True,  auto_mode_trigger_on_image_in_page=True,
+)
+
+documents = SimpleDirectoryReader(
+    input_dir="./docs", 
+    file_extractor={".pdf": parser}
+).load_data()
+print("docs ingested")
+print(documents[0].text[:100])
